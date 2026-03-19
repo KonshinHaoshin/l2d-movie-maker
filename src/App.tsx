ffe2867845fcd477919c346dbef33b2c6e631e58
KonshinHaoshin <konshinhaoshin@gmail.com>
2026-03-19 17:46:55 +0800

    fix live2d canvas bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Live2DView from "./components/Live2DView";
 function App() {
 
     return (
-        <div style={{ width: "100vw", height: "100vh", background: "#111" }}>
+        <div style={{ width: "100%", height: "100%", overflow: "hidden", background: "#111" }}>
             <Live2DView />
         </div>
     );
